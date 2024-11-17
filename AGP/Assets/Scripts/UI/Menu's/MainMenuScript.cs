2e7545f73ad71b17bb071cd47126b8b7accbb428
Yonik-Vinegar <Yannick5215@gmail.com>
2024-11-17 19:55:22 +0000

    Main Menu nearly finished.
    started settings menu as well.

---
+++ AGP/Assets/Scripts/UI/Menu's/MainMenuScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
