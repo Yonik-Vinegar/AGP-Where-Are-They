581a14ba6d75f3883011384ce858e103b4192cf7
YonikVinegar <71383996+Yonik-Vinegar@users.noreply.github.com>
2025-07-12 18:30:14 +0100

    console models in

--- AGP/Assets/Scripts/Player/PlayerLocomation.cs
+++ AGP/Assets/Scripts/Player/PlayerLocomation.cs
@@ -15,12 +15,18 @@ public class PlayerLocomation : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody playerRigidBody;
     public float rotationSpeed = 15;
-    public float fallSpeed =0.4f;
+    public float fallSpeed;
 
-    //[Header("Slope Handling")]
-    //public float maxSlopeAngle;
-    //private RaycastHit slopeHit;
-    //public float playerHeight;
+    //private float playerHeight;
+    //public GameObject Player;
+
+    //[Header("Grounded Detection")]
+    //public bool isGrounded;
+    //private float groundDistance = 0.4f;
+    //[SerializeField] LayerMask groundMask;
+    //RaycastHit slopeHit;
+    //Vector3 SlopeMoveDirection;
+
 
 
     public void Awake()
@@ -28,6 +34,7 @@ public class PlayerLocomation : MonoBehaviour
         cameraObject =  Camera.main.transform;
         inputManager = GetComponent<InputManager>();
         playerRigidBody = GetComponent<Rigidbody>();
+        //playerHeight = Player.transform.localScale.y;
     }
 
     public void HandleAllMovement()
@@ -38,49 +45,76 @@ public class PlayerLocomation : MonoBehaviour
 
     private void HandleMovement()
     {
+        //if (isGrounded && !Onslope())
+        //{
+        //    moveDirection = cameraObject.forward * inputManager.verticalInput;
+        //    moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        //    moveDirection.Normalize();
+        //    moveDirection.y = -fallSpeed;
+        //    moveDirection = moveDirection * movementSpeed;
+
+        //    Vector3 movementVelocity = moveDirection;
+        //    playerRigidBody.velocity = movementVelocity;
+        //}
+        //else if (isGrounded && Onslope())
+        //{
+        //    SlopeMoveDirection = cameraObject.forward * inputManager.verticalInput;
+        //    SlopeMoveDirection = SlopeMoveDirection + cameraObject.right * inputManager.horizontalInput;
+        //    SlopeMoveDirection.Normalize();
+        //    SlopeMoveDirection.y = -fallSpeed;
+        //    SlopeMoveDirection = SlopeMoveDirection * movementSpeed;
+
+        //    Vector3 movementVelocity = SlopeMoveDirection;
+        //    playerRigidBody.velocity = movementVelocity;
+        //}
+        //else if (!isGrounded)
+        //{
+
+        //}
         moveDirection = cameraObject.forward * inputManager.verticalInput;
         moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
         moveDirection.Normalize();
-        moveDirection.y = - fallSpeed;
+        moveDirection.y = -fallSpeed;
         moveDirection = moveDirection * movementSpeed;
 
         Vector3 movementVelocity = moveDirection;
         playerRigidBody.velocity = movementVelocity;
-
-        // handling slope
-        //if (Onslope())
-        //{
-        //    playerRigidBody.AddForce(GetSlopeMoveDirection() * movementSpeed * 20f, ForceMode.Force);
-        //}
     }
+
+    //private void update()
+    //{
+    //    isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, 1, 0), groundDistance, groundMask);
+
+    //    SlopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+    //}
 
     //private bool Onslope()
     //{
-    //    if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f ))
+    //    if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
     //    {
-    //        float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
-    //        return angle < maxSlopeAngle && angle != 0;
-    //        Debug.Log("onslope");
+    //       if (slopeHit.normal != Vector3.up)
+    //       {
+    //         return true;
+    //       }
+    //       else
+    //       {
+    //         return false;
+    //       }
     //    }
-
     //    return false;
     //}
 
-    //private Vector3 GetSlopeMoveDirection()
-    //{
-    //    return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
-    //}
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Slope"))
         {
             fallSpeed = 0.1f;
         }
-        
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        fallSpeed = 0.6f;
+        fallSpeed = 0;
     }
 }
